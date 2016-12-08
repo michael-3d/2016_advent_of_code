737016272172d67d02499cd7076124ccf759b236
Michael Sweet <michael@3dmarcomms.com>
2016-12-08 17:05:04 +0000

    day 3 part 2

--- aoc_3.erl
+++ aoc_3.erl
@@ -1,8 +1,11 @@
 -module(aoc_3).
--export([part1/1]).
+-export([part1/1, part2/1]).
 
 part1(Input) ->
 	add(Input, 0).
+
+part2(Input) ->
+	add(lists:append(lists:append(shake(Input, [], 1), shake(Input, [], 2)), shake(Input, [], 3)), 0).
 
 add([], C) -> C;
 add([I | Input], C) ->
@@ -18,7 +21,12 @@ validate(T) ->
 			0
 	end.
 	 
-	
+shake([], New, _) -> New;
+shake(Old, New, N) ->
+	L1 = e(N, lists:nth(1, Old)),
+	L2 = e(N, lists:nth(2, Old)),
+	L3 = e(N, lists:nth(3, Old)),
+	shake(lists:sublist(Old, 4, length(Old)), [{L1, L2, L3} | New], N).
 
 e(N, T) ->
 	element(N, T).
